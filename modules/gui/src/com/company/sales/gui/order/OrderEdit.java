86aae64c2653956eb4652770c2373dab1f09fda2
Gleb Gorelov <glebfox@gmail.com>
2015-11-20 15:24:00 +0400

    Apache 2.0 License

--- modules/gui/src/com/company/sales/gui/order/OrderEdit.java
+++ modules/gui/src/com/company/sales/gui/order/OrderEdit.java
@@ -1,22 +1,31 @@
 /*
- * Copyright (c) 2014 Haulmont
+ * Copyright (c) 2015 Haulmont
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.company.sales.gui.order;
 
+import com.company.sales.entity.Order;
 import com.company.sales.entity.OrderLine;
 import com.haulmont.cuba.gui.WindowManager;
 import com.haulmont.cuba.gui.components.AbstractEditor;
-import com.company.sales.entity.Order;
 import com.haulmont.cuba.gui.components.actions.CreateAction;
 import com.haulmont.cuba.gui.components.actions.EditAction;
 import com.haulmont.cuba.gui.data.CollectionDatasource;
-import com.haulmont.cuba.gui.data.CollectionDatasourceListener;
-import com.haulmont.cuba.gui.data.impl.CollectionDsListenerAdapter;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
